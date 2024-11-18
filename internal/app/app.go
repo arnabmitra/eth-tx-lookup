b2fef187923521afe7a910af16677303cc8b8ea3
Arnab Mitra <arnabmitra@arnabs-air.lan>
2024-11-17 21:19:36 -0700

    adding routes in a different way.

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,12 +2,15 @@ package app
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -53,13 +56,12 @@ func (a *App) Start(ctx context.Context) error {
 
 	tmpl := template.Must(template.New("").ParseGlob("./templates/*"))
 
-	a.loadRoutes(tmpl)
+	a.loadRoutes()
 
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
 		Handler: middleware.Logging(a.logger, middleware.HandleBadCode(tmpl, a.router)),
 	}
-
 	done := make(chan struct{})
 	go func() {
 		err := server.ListenAndServe()
@@ -69,7 +71,7 @@ func (a *App) Start(ctx context.Context) error {
 		close(done)
 	}()
 
-	a.logger.Info("Server listening", slog.String("addr", ":8080"))
+	a.logger.Info("Server listening!!!", slog.String("addr", ":8080"))
 	select {
 	case <-done:
 		break
@@ -80,4 +82,91 @@ func (a *App) Start(ctx context.Context) error {
 	}
 
 	return nil
+}
+func ethTxHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		txHash := r.FormValue("txhash")
+		details, err := fetchTxDetails(txHash)
+		if err != nil {
+			http.Error(w, "Failed to fetch transaction details", http.StatusInternalServerError)
+			return
+		}
+
+		tmpl, err := template.ParseFiles("templates/eth.html")
+		if err != nil {
+			http.Error(w, "Failed to load template", http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.Execute(w, struct {
+			TxHash  string
+			Details *AlchemyResponse
+		}{TxHash: txHash, Details: details})
+	} else {
+		tmpl, err := template.ParseFiles("templates/eth.html")
+		if err != nil {
+			http.Error(w, "Failed to load template", http.StatusInternalServerError)
+			return
+		}
+
+		tmpl.Execute(w, nil)
+	}
+}
+
+type AlchemyResponse struct {
+	Jsonrpc string `json:"jsonrpc"`
+	Id      int    `json:"id"`
+	Result  struct {
+		BlockHash        string `json:"blockHash"`
+		BlockNumber      HexInt `json:"blockNumber"`
+		From             string `json:"from"`
+		Gas              string `json:"gas"`
+		GasPrice         string `json:"gasPrice"`
+		Hash             string `json:"hash"`
+		Input            string `json:"input"`
+		Nonce            string `json:"nonce"`
+		To               string `json:"to"`
+		TransactionIndex string `json:"transactionIndex"`
+		Value            HexInt `json:"value"`
+		V                string `json:"v"`
+		R                string `json:"r"`
+		S                string `json:"s"`
+	} `json:"result"`
+}
+type HexInt int64
+
+func (h *HexInt) UnmarshalJSON(data []byte) error {
+	// Remove the quotes around the JSON string
+	hexStr := strings.Trim(string(data), "\"")
+	if len(hexStr) > 2 && hexStr[:2] == "0x" {
+		hexStr = hexStr[3:]
+	}
+	// Convert the hexadecimal string to an integer
+	intValue, err := strconv.ParseInt(hexStr, 16, 64)
+	if err != nil {
+		return err
+	}
+	*h = HexInt(intValue)
+	return nil
+}
+
+func fetchTxDetails(txHash string) (*AlchemyResponse, error) {
+
+	apiKey := os.Getenv("ETH_API_KEY")
+	url := fmt.Sprintf("https://eth-mainnet.alchemyapi.io/v2/%s", apiKey)
+
+	payload := fmt.Sprintf(`{"jsonrpc":"2.0","method":"eth_getTransactionByHash","params":["%s"],"id":1}`, txHash)
+	resp, err := http.Post(url, "application/json", strings.NewReader(payload))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var alchemyResponse AlchemyResponse
+	if err := json.NewDecoder(resp.Body).Decode(&alchemyResponse); err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("The response from alchemy is %v \n ", alchemyResponse)
+	return &alchemyResponse, nil
 }
